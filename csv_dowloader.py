597dcb4911092c61cc6d113143052d328a1cf78e
johnny23@live.com.sg <johnny23@live.com.sg>
2023-02-28 10:28:18 +0800

    switched to python 3.9 config

--- csv_dowloader.py
+++ csv_dowloader.py
@@ -2,7 +2,7 @@ from stockrow_scraper_api import get_stockrow_data
 from file_mover import format_copy_file
 import time
 
-ticker = "baba"
+ticker = "PDD"
 
 get_stockrow_data(ticker)
 time.sleep(3)

04f248d840cbb1f9e1654ae7c913242dcc14bea8
johnny <johnny@napierhealthcare.com>
2023-10-31 10:39:01 +0800

    fixed file paths for running CSV download as mod

--- modules/file_mover.py
+++ modules/file_mover.py
@@ -2,9 +2,12 @@ import os
 import glob
 import shutil
 
-def format_copy_file()->None:
+file_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+def format_copy_file() -> None:
     # Read directories from txt file and clean them
-    lines = open("download_and_end_folders.txt",'r').readlines()
+    lines = open(f"{file_path}\download_and_end_folders.txt", "r").readlines()
     modified = []
     for line in lines:
         if line[-1] == "\n":
@@ -12,34 +15,36 @@ def format_copy_file()->None:
         else:
             modified.append(line)
 
-    #assign source and destination folder
+    # assign source and destination folder
     download_folder = modified[1]
     destination_folder = modified[4]
 
-    #access download folder and retain only the financial files
+    # access download folder and retain only the financial files
     os.chdir(download_folder)
+
     financial_tables = []
     for file in os.listdir():
-        if "financials" in file: 
+        if "financials" in file:
             financial_tables.append(file)
 
-    #sort financial files and create mapped pair 
+    # sort financial files and create mapped pair
     financial_tables = sorted(financial_tables)
     mapped_title = ["balance_sheet", "cash_flow", "metrics", "growth", "income"]
 
-    #rename downloaded files
+    # rename downloaded files
     count = 0
     for file in financial_tables:
         try:
-            os.rename(file, mapped_title[count]+".xlsx")
+            os.rename(file, mapped_title[count] + ".xlsx")
         except FileExistsError:
-            os.remove(mapped_title[count]+".xlsx")
-            os.rename(file, mapped_title[count]+".xlsx")
+            os.remove(mapped_title[count] + ".xlsx")
+            os.rename(file, mapped_title[count] + ".xlsx")
         count += 1
 
-    #copy all files over to destination folder
+    # copy all files over to destination folder
     for file in os.listdir():
         shutil.copy(file, destination_folder)
 
-if __name__ =="__main__":
+
+if __name__ == "__main__":
     format_copy_file()
